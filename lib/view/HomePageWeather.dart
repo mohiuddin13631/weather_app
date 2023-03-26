a59e9276090ea3bfcdaab4b7f4416cb823613b4e
Mohiuddin Khan <60233974+mohiuddin13631@users.noreply.github.com>
2023-03-26 20:57:02 +0600

    pasted manually

--- lib/view/HomePageWeather.dart
+++ lib/view/HomePageWeather.dart
@@ -13,7 +13,8 @@ class HomePageWeather extends StatefulWidget {
   State<HomePageWeather> createState() => _HomePageWeatherState();
 }
 
-class _HomePageWeatherState extends State<HomePageWeather> {
+class _HomePageWeatherState extends State<HomePageWeather> with SingleTickerProviderStateMixin{
+
 
   _determinePosition() async {
     bool serviceEnabled;
@@ -49,28 +50,39 @@ class _HomePageWeatherState extends State<HomePageWeather> {
 
   Map<String, dynamic>? weatherData;
   Map<String, dynamic>? weatherForecast;
+  Map<String, dynamic>? weeklyForecast;
 
 
   weatherApiCall() async
   {
     String weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=${position!.latitude}&lon=${position!.longitude}&appid=bf881cd668c8bd1799a36d79a42c897b&units=metric&lang=English";
     String forCastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=${position!.latitude}&lon=${position!.longitude}&appid=bf881cd668c8bd1799a36d79a42c897b&units=metric";
+    String weeklyForecastUrl = "http://api.weatherapi.com/v1/forecast.json?key= 4a536faf63724b8a86a50958232603 &q=Dhaka&days=7&aqi=no&alerts=no";
+    //todo: Current weather------------------------------------
     var weatherResponse = await http.get(Uri.parse(weatherUrl));
     // final data = json.decode(response.body);
     var decodeWeatherData = jsonDecode(weatherResponse.body);
-
+    //todo: forecast------------------------------------------
     var forCastResponse = await http.get(Uri.parse(forCastUrl));
     var decodeForCastData = jsonDecode(forCastResponse.body);
+    //todo: weekly forecast
+    var weeklyForecasResponse =await http.get(Uri.parse(weeklyForecastUrl));
+    var decodeWeeklyForcastData = jsonDecode(weeklyForecasResponse.body);
     setState(() {
       weatherData = Map<String,dynamic>.from(decodeWeatherData);
 
       weatherForecast = Map<String,dynamic>.from(decodeForCastData);
+
+      weeklyForecast = Map<String,dynamic>.from(decodeWeeklyForcastData);
     });
 
   }
+
+  TabController? tabController;
   @override
   void initState() {
     // TODO: implement initState
+    tabController = TabController(length: 2, vsync: this,initialIndex: 0);
     _determinePosition();
 
     Timer.periodic(const Duration(minutes: 2), (timer) {
@@ -106,7 +118,23 @@ class _HomePageWeatherState extends State<HomePageWeather> {
               Positioned(
                 top: 50,
                 right: 10,
-                child: Text("Country ${weatherData!['sys']['country']}",style: myTextStyle(),),),
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Country ',
+                    style: TextStyle(color: Colors.white),
+                    children: [
+                      TextSpan(
+                        text: '${weatherData!['sys']['country']}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
               Column(
                 children: [
                   SizedBox(height: 98,),
@@ -117,8 +145,7 @@ class _HomePageWeatherState extends State<HomePageWeather> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-
-                      Text("${weatherData!['main']['temp'].toInt()}°",style: myTextStyle(65,FontWeight.w200),),
+                      Text("${weatherData!['main']['temp'].toInt()}°",style: myTextStyle(70,FontWeight.w200),),
                       Image.network("https://openweathermap.org/img/wn/${weatherData!['weather'][0]['icon']}@2x.png",color: Colors.white,),
                       SizedBox(width: 45,),
                     ],
@@ -162,10 +189,25 @@ class _HomePageWeatherState extends State<HomePageWeather> {
                   ),
                   child: Column(
                     children: [
-                      const SizedBox(height: 20,),
-                      Text("Hourly forecast",style:TextStyle(color: Color(0xffEBEBF5).withOpacity(.6),fontSize: 16,fontWeight: FontWeight.w600),),
+                      // const SizedBox(height: 20,),
+                      TabBar(
+
+                        labelColor: Colors.white,
+                        unselectedLabelColor: Color(0xffEBEBF5).withOpacity(.6),
+                        indicatorWeight: 1.0,
+                        // indicatorSize: TabBarIndicatorSize.label,
+                        indicatorColor: Color(0xffEBEBF5).withOpacity(.8),
+                        controller: tabController,
+
+                        tabs: [
+                          Tab(text: "Hourly Forecast",),
+                          Tab(text: "Weekly Forecast",)
+                        ],
+
+                      ),
+                      // Text("Hourly forecast",style:TextStyle(color: Color(0xffEBEBF5).withOpacity(.6),fontSize: 16,fontWeight: FontWeight.w600),),
                       // Text("Hourly forecast",style:TextStyle(color: Colors.white.withOpacity(.6),fontSize: 16),)
-                      const SizedBox(height: 10,),
+                      // const SizedBox(height: 10,),
                       Container(
                         width: double.infinity,
                         height: 1,
@@ -173,43 +215,109 @@ class _HomePageWeatherState extends State<HomePageWeather> {
                       ),
                       SizedBox(height: 15,),
 
-                      Container(
-                        margin: EdgeInsets.only(left: 20),
-                        width: double.infinity,
-                        height: 146,
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          scrollDirection: Axis.horizontal,
-                          itemCount: weatherForecast!['list'].length,
-                          itemBuilder: (context, index) {
-                            // var vlue = Jiffy(weatherForecast!['list'][index]['dt_txt']).format('h');
-                            // var time = int.parse(vlue);
-                            // print(time);
-                            return SizedBox(
-                              width: 65,
-                                height: 146,
-                              child: Card(
-                                // color: Color(0xff48319D),
-                                color:Color(0xff48319D).withOpacity(.2),
-                                // color:Colors.blue,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  side: const BorderSide(color: Colors.white,width: 1)
-                                ),
-                                // elevation: 0,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(Jiffy(weatherForecast!['list'][index]['dt_txt']).format('h a'),style: TextStyle(color: Colors.white.withOpacity(1)),),
-                                    Text(Jiffy(weatherForecast!['list'][index]['dt_txt']).format('d MMM'),style: TextStyle(fontSize: 12,color: Colors.white.withOpacity(.8)),),
-                                    Image.network("https://openweathermap.org/img/wn/${weatherForecast!['list'][index]['weather'][0]['icon']}@2x.png",),
-                                    Text("${weatherForecast!['list'][index]['main']['temp'].toInt()}°",style: TextStyle(color: Colors.white.withOpacity(.8)),),
-                                  ],
-                                ),
-                              ),
-                            );
-                        },),
+                      Flexible(
+                        child: TabBarView(
+                          controller: tabController,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              width: double.infinity,
+                              height: 146,
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemCount: weatherForecast!['list'].length,
+                                itemBuilder: (context, index) {
+                                  // var vlue = Jiffy(weatherForecast!['list'][index]['dt_txt']).format('h');
+                                  // var time = int.parse(vlue);
+                                  // print(time);
+                                  return SizedBox(
+                                    width: 65,
+                                    height: 146,
+                                    child: Card(
+                                      // color: Color(0xff48319D),
+                                      color:Color(0xff48319D).withOpacity(.2),
+                                      // color:Colors.blue,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(30),
+                                          side: const BorderSide(color: Colors.white,width: 1)
+                                      ),
+                                      // elevation: 0,
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(vertical: 7),
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            Text(Jiffy(weatherForecast!['list'][index]['dt_txt']).format('h a'),style: TextStyle(color: Colors.white.withOpacity(1)),),
+                                            Text(Jiffy(weatherForecast!['list'][index]['dt_txt']).format('d MMM'),style: TextStyle(fontSize: 12,color: Colors.white.withOpacity(.8)),),
+                                            Text(weatherForecast!['list'][index]['weather'][0]['main'],style: TextStyle(color: Colors.white),),
+                                            Image.network("https://openweathermap.org/img/wn/${weatherForecast!['list'][index]['weather'][0]['icon']}@2x.png",),
+                                            Text("${weatherForecast!['list'][index]['main']['temp'].toInt()}°",style: TextStyle(color: Colors.white.withOpacity(.8)),),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              width: double.infinity,
+                              height: 146,
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemCount: weeklyForecast!['forecast']['forecastday'].length,
+                                itemBuilder: (context, index) {
+                                  // var vlue = Jiffy(weatherForecast!['list'][index]['dt_txt']).format('h');
+                                  // var time = int.parse(vlue);
+                                  // print(time);
+                                  return SizedBox(
+                                    width: 120,
+                                    height: 146,
+                                    child: Card(
+                                      // color: Color(0xff48319D),
+                                      color:Color(0xff48319D).withOpacity(.2),
+                                      // color:Colors.blue,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(25),
+                                          side: const BorderSide(color: Colors.white,width: 1)
+                                      ),
+                                      // elevation: 0,
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                                        children: [
+                                          Text(Jiffy(weeklyForecast!['forecast']['forecastday'][index]['date']).format('d MMM'),style: TextStyle(color: Colors.white.withOpacity(1)),),
+                                          // Image.network("${weeklyForecast!['forecast']['forecastday'][index]['day']['condition']['icon']}",scale: 2,),
+                                          Text("Avg ${weeklyForecast!['forecast']['forecastday'][index]['day']['avgtemp_c'].toInt()} °C",style: TextStyle(fontSize: 16,color: Colors.white.withOpacity(1)),),
+                                          // Image.network("https://openweathermap.org/img/wn/${weatherForecast!['list'][index]['weather'][0]['icon']}@2x.png",),
+                                          Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                              Text("Sunrise",style: TextStyle(color: Colors.white.withOpacity(.8)),),
+                                              Image.asset("assets/images/sunrise_32.png",scale: 1.5,),
+                                            ],
+                                          ),
+                                          Text("${weeklyForecast!['forecast']['forecastday'][index]['astro']['sunrise']}",style: TextStyle(color: Colors.white),),
+                                          Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                              Text("Sunset",style: TextStyle(color: Colors.white.withOpacity(.8)),),
+                                              Image.asset("assets/images/sunset_32.png",scale: 1.5,),
+                                            ],
+                                          ),
+                                          Text("${weeklyForecast!['forecast']['forecastday'][index]['astro']['sunset']}",textAlign:TextAlign.end,style: TextStyle(color: Colors.white),),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },),
+                            ),
+                          ],
+                        ),
                       ),
+
 
 
                       Padding(
